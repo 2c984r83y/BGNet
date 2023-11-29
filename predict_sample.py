1c77a69e06790f66e96702276cf73f3cbc2e26ed
2c984r83y <1422512438@qq.com>
2023-11-29 15:41:05 +0800

    Update image resizing and add timing measurement

--- predict_sample.py
+++ predict_sample.py
@@ -20,14 +20,17 @@ from PIL import Image
 from datasets.data_io import get_transform
 from models.bgnet import BGNet
 from models.bgnet_plus import BGNet_Plus
+import time
 
 model = BGNet_Plus().cuda()
 
 checkpoint = torch.load('models/Sceneflow-IRS-BGNet-Plus.pth',map_location=lambda storage, loc: storage)
 model.load_state_dict(checkpoint) 
 model.eval()
-left_img = Image.open('sample/im0.png').convert('L')
-right_img = Image.open('sample/im1.png').convert('L')
+# left_img1 = Image.open('sample/im0.png').convert('L')
+# right_img = Image.open('sample/im1.png').convert('L')
+left_img = Image.open('/disk2/users/M22_zhaoqinghao/PSMNet/testing/desc_l_1.png').convert('L')
+right_img = Image.open('/disk2/users/M22_zhaoqinghao/PSMNet/testing/desc_r_1.png').convert('L')
 w, h = left_img.size
 h1 = h % 64
 w1 = w % 64
@@ -35,13 +38,24 @@ h1 = h  - h1
 w1 =  w - w1
 h1 = int(h1)
 w1 = int(w1)
-left_img = left_img.resize((w1, h1),Image.ANTIALIAS)
-right_img = right_img.resize((w1, h1),Image.ANTIALIAS)
+# left_img = left_img.resize((w1, h1),Image.ANTIALIAS)
+# right_img = right_img.resize((w1, h1),Image.ANTIALIAS)
+left_img = left_img.resize((w1, h1),Image.Resampling.LANCZOS)
+right_img = right_img.resize((w1, h1),Image.Resampling.LANCZOS)
 left_img = np.ascontiguousarray(left_img, dtype=np.float32)
 right_img = np.ascontiguousarray(right_img, dtype=np.float32)
 preprocess = get_transform()
 left_img = preprocess(left_img)
 right_img = preprocess(right_img)
-pred,_ = model(left_img.unsqueeze(0).cuda(), right_img.unsqueeze(0).cuda()) 
+# GPU dry run
+with torch.no_grad():
+    pred,_ = model(left_img.unsqueeze(0).cuda(), right_img.unsqueeze(0).cuda()) 
+    pred,_ = model(left_img.unsqueeze(0).cuda(), right_img.unsqueeze(0).cuda()) 
+    pred,_ = model(left_img.unsqueeze(0).cuda(), right_img.unsqueeze(0).cuda()) 
+time_start=time.time()
+with torch.no_grad():
+    pred,_ = model(left_img.unsqueeze(0).cuda(), right_img.unsqueeze(0).cuda()) 
+time_end=time.time()
+print('time cost: ',time_end-time_start,'s')
 pred = pred[0].data.cpu().numpy() * 256   
 skimage.io.imsave('sample_disp.png',pred.astype('uint16'))
